0cc63ce803a6f5a8bf6993dff5def48a36f2e2e7
Lucas Stirpe <49053857+lucasnahuels@users.noreply.github.com>
2019-03-31 00:42:39 -0300

    Add files via upload

---
+++ html/AltaProfesorOk.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html> 
+<head>
+	<title> Envio correcto</title>
+
+	<style type="text/css">
+		body{
+			background: url('../html/Crossfit-Wallpaper.jpg');
+		}
+	</style>
+</head>
+
+<body>
+
+<a style="position:absolute; top:10px; right:20px ;color:white; font-weight: bold; font-size: 20px;"  href="logoutgym.php"> Cerrar sesión</a>
+<h3 style="color: red; font-weight: bold; font-size: 25px; background-color: black; display: inline; position:absolute; margin-top: 20px" > Los datos fueron enviados correctamente</h3>
+<br/><br/><br/>
+<a href="crearprofesor.php" style="color: red; font-weight: bold; font-size: 20px; background-color: black"> Volver </a>
+
+</body>
+</html>
